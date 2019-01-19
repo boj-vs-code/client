ad4ca974bb17cacf7aa09d1dfed4af1a3305e0a3
moreal <dev.moreal@gmail.com>
2019-01-19 20:54:31 +0900

    Change import method like typescript

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
 import * as process from "process";
-import * as lib from "./lib";
-
-const toLanguage = lib.toLanguage;
+import { toLanguage, isNumber } from "./lib";
 
 const argv = process.argv.slice(2);
 
 const filename = argv[0];
 const extname = argv[1].slice(1);
+const language = toLanguage(extname);
 const dirname = argv[2].split('/').slice(0, -1).reverse()[0];
 
 if (filename === "solve") { // directory will have the problem number, if else
     // TODO
-} else if (NaN !== Number(filename)) {  // if filename is problem number
+} else if (isNumber(filename)) {  // if filename is problem number
     
-} else if (filename.indexOf('-') !== -1 && NaN !== Number(filename.split('-')[0])) { // if filename has - to notice problem name
+} else if (filename.indexOf('-') !== -1 && isNumber(filename.split('-')[0])) { // if filename has - to notice problem name
     
 } else {
     // vscode.window.showErrorMessage("BOJ Can't know the problem number!");
