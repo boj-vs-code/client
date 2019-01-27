8481e9c1d730aeb5df1166e263ebaf7d58f47d7a
moreal <dev.moreal@gmail.com>
2019-01-28 02:18:49 +0900

    Add TestCase class and Problem class

---
+++ src/api/boj.ts
@@ -0,0 +1,56 @@
+class Account {
+    private id: string
+    private pw: string
+
+    constructor(id: string, pw: string) {
+        this.id = id
+        this.pw = pw
+    }
+
+    public getId(): string {
+        return this.id
+    }
+
+    public getPw(): string {
+        return this.pw
+    }
+}
+
+class TestCase {
+    public input: string
+    public output: string
+
+    constructor(input: string, output: string) {
+        this.input = input
+        this.output = output
+    }
+}
+
+class Problem {
+    private description: string
+    private inputDescription: string
+    private outputDescription: string
+    private testcases: Array<TestCase>
+
+}
+
+export class BOJSession {
+    private sessionId: string
+    private account: Account
+    
+    constructor(id: string, pw: string) {
+        this.account = new Account(id, pw)
+    }
+
+    public login() {
+
+    }
+
+    public isLoggedIn() {
+
+    }
+
+    public submit(problem: Number) {
+        
+    }
+}
