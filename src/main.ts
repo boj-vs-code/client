dd90aaf342cd12ec934161a9049d6c1c4d324424
moreal <dev.moreal@gmail.com>
2019-02-04 11:59:59 +0900

    Remove useless code from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,13 @@
 import * as process from "process";
-import { toLanguage, isNumber, hasProblemNumberAndOthers } from "./lib";
+import {} from "./lib";
 import BOJ, { BOJSession, Language } from "./api/boj";
-import { O_SYNC } from "constants";
 
 const argv = process.argv.slice(2);
 
 const filename = argv[0];
 const extname = argv[1].slice(1);
-const language = toLanguage(extname);
+// const language = toLanguage(extname);
 const dirname = argv[2].split('/').slice(0, -1).reverse()[0];
-
-if (filename === "solve") {
-    
-} else if (isNumber(filename)) {
-    
-} else if (hasProblemNumberAndOthers(filename)) { // if filename has - to notice problem name
-    
-} else {
-    // vscode.window.showErrorMessage("BOJ Can't know the problem number!");
-    // SHOW WARNING ERROR: Cant know problem number
-}
 
 (async () => {
     const session = new BOJSession();
@@ -29,6 +17,6 @@ if (filename === "solve") {
         session.submit(1000, new Language('python', 28), 'print(sum(list(map(int, input().split()))))')
     }, 2000);
     // console.log(problem);
-})()
+})
 
 console.log(filename, language, dirname);
