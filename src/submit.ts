3b865601d370c3896037f66255785cc70549e92e
moreal <dev.moreal@gmail.com>
2019-02-11 21:09:06 +0900

    Update src/submit.ts to remove useless debug log

--- src/submit.ts
+++ src/submit.ts
@@ -5,14 +5,8 @@ import { BOJSession } from "./api/boj";
 async function submitBOJ(session: BOJSession) {
   vscode.window.showInformationMessage("Let's submit code xD");
 
-  vscode.window.showInformationMessage("data0?");
-
   const languages = getLanguages();
-
-  vscode.window.showInformationMessage("data1?");
   const languageNames = languages.map(x => x.language);
-
-  vscode.window.showInformationMessage("data2?");
   const regexp = new RegExp(`^${getLanguage()}(\\s|$|\\d)+`);
   const filtered = languageNames.filter(
     x =>
@@ -24,8 +18,6 @@ async function submitBOJ(session: BOJSession) {
         .search(regexp) !== -1
   );
 
-  vscode.window.showInformationMessage("data3?");
-
   let selectedLanguage: string | undefined;
   if (filtered.length === 1) {
     selectedLanguage = filtered[0];
@@ -34,21 +26,14 @@ async function submitBOJ(session: BOJSession) {
       0 === filtered.length ? languageNames : filtered
     );
   }
-  vscode.window.showInformationMessage("data3?");
-  //   vscode.window.showInformationMessage(<string>selectedLanguage);
 
   if (selectedLanguage === undefined) {
     vscode.window.showErrorMessage("You didn't select the Language");
     return;
   }
 
-  // get language
   const language = languages.filter(x => x.language === selectedLanguage)[0];
-
-  // get problem
   const problemNumber = getProblemNumber();
-
-  // get source
   const source = getSource();
 
   await session.submit(problemNumber, language, source);
