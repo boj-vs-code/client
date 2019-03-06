53bd1c6e3dc02c0cf9f617f5725d11fb3f5c8b34
moreal <dev.moreal@gmail.com>
2019-03-06 11:14:59 +0900

    Add testcode for BOJ#getProblem

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,6 +5,9 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from "assert";
+import BOJ from "../api/boj";
+import { Problem } from "../api/boj/problem";
+import { TestCase } from "../api/boj/testcase";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -12,10 +15,19 @@ import * as assert from "assert";
 // import * as myExtension from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function() {
+suite("BOJ API Tests", function() {
   // Defines a Mocha unit test
-  test("Something 1", function() {
-    assert.equal(-1, [1, 2, 3].indexOf(5));
-    assert.equal(-1, [1, 2, 3].indexOf(0));
+  test("Get problem 1000", async () => {
+    const parsedProblem = await BOJ.getProblem(1000);
+    const problem1000 = new Problem(
+      "A+B",
+      "두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.",
+      "첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)",
+      "첫째 줄에 A+B를 출력한다.",
+      [new TestCase("1 2", "3")],
+      {}
+    );
+
+    assert.equal(parsedProblem, problem1000);
   });
 });
