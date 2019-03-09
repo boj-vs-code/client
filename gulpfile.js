2689a1eae8e176d0ae2216967bab445972e2bc5d
moreal <dev.moreal@gmail.com>
2019-03-09 21:21:28 +0900

    Add gulp package

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+"use strict"
+
+var gulp = require('gulp');
+
+gulp.task('move-resources', function(){
+    return gulp.src(['./src/test/resources/**/*', './src/test/resources/**/.*'])
+                    .pipe(gulp.dest('./out/test/resources/'));
+});
