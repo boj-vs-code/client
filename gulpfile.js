0f3acf459d995c9a7ea253858ce02ee1e2c28763
moreal <dev.moreal@gmail.com>
2019-03-09 22:25:00 +0900

    Format gulpfile.js using prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-"use strict"
+"use strict";
 
-var gulp = require('gulp');
+var gulp = require("gulp");
 
-gulp.task('move-resources', function(){
-    return gulp.src(['./src/test/resources/**/*', './src/test/resources/**/.*'])
-                    .pipe(gulp.dest('./out/test/resources/'));
+gulp.task("move-resources", function() {
+  return gulp
+    .src(["./src/test/resources/**/*", "./src/test/resources/**/.*"])
+    .pipe(gulp.dest("./out/test/resources/"));
 });
