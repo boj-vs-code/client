7d951caf2e5db95b35b7487e0e712914f228096e
moreal <dev.moreal@gmail.com>
2019-03-10 22:49:03 +0900

    Append BOJScoringColor elements to reshape Submit Tasks View

--- src/api/boj/enums/scoring-status-color.ts
+++ src/api/boj/enums/scoring-status-color.ts
@@ -1,8 +1,11 @@
 export enum ScoringStatusColor {
-  WAITING = "wait",
-  CORRECT = "ac",
-  WRONG_ANSWER = "wa",
-  RUNTIME_ERROR = "rte",
-  MEMORY_OVER = "mle",
-  COMPILE_ERROR = "ce"
+  WAITING = "#95a5a6",
+  JUDGE_WAITING = "#e67e22",
+  JUDGING = "#e67e22",
+  CORRECT = "#009F6B",
+  WRONG_ANSWER = "#e74c3c",
+  RUNTIME_ERROR = "#4765a0",
+  PRINT_FORMAT_ERROR = "#e74c3c",
+  MEMORY_OVER = "#e74c3c",
+  COMPILE_ERROR = "#4765a0"
 }
