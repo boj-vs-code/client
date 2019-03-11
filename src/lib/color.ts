6b607207029c6710ec4f714aa782cc7b84aa132f
moreal <dev.moreal@gmail.com>
2019-03-11 15:08:04 +0900

    Implements lib/color#getColorFromScoringStatus

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,13 +1,7 @@
 import { ScoringStatusColor } from "../api/boj/enums/scoring-status-color";
 import { ScoringStatus } from "../api/boj/enums/scoring-status";
 
-export function getColorFromScoringStatus(
-  code: ScoringStatus
-): ScoringStatusColor {
-  for (const key in ScoringStatusColor) {
-    if (((ScoringStatus[key as any] as any) as ScoringStatus) === code) {
-      return (ScoringStatusColor[key] as any) as ScoringStatusColor;
-    }
-  }
-  return ScoringStatusColor.WAITING;
+export function getColorFromScoringStatus(code: ScoringStatus): string {
+  const status = ScoringStatus[code];
+  return ScoringStatusColor[status as any];
 }
