9ee94ddedbf25f5a3d15d12c82e85e5e76d518c5
moreal <dev.moreal@gmail.com>
2019-03-12 22:26:30 +0900

    Update testcode by some change entire architecture

--- src/test/api/boj/submit-task.test.ts
+++ src/test/api/boj/submit-task.test.ts
@@ -1,12 +1,14 @@
 import * as assert from "assert";
-import { SubmitTaskManager } from "../../../api/boj/submit-task";
+import { SubmitTaskManager } from "../../../api/boj/managers/submit-task";
 
 suite("BOJ API TEST at api/boj/taskss", () => {
   suite("SubmitTaskManager#tasks", () => {
     test("1, 2, 3 => 3, 2, 1 // when limit is 3", () => {
       const testElements = [1, 2, 3];
-      testElements.forEach(el => SubmitTaskManager.createTask(`${el}`, el));
-      const tasks = SubmitTaskManager.tasks;
+      testElements.forEach(el =>
+        SubmitTaskManager.getInstance().createTask(`${el}`, el)
+      );
+      const tasks = SubmitTaskManager.getInstance().tasks;
       assert.equal(tasks.length, testElements.length);
       testElements
         .reverse()
@@ -14,16 +16,17 @@ suite("BOJ API TEST at api/boj/taskss", () => {
     });
 
     test("1, 2, 3, 4, 5 => 5, 4, 3 // when limit is 3", () => {
+      SubmitTaskManager.getInstance().clear();
+
       const testElements = [1, 2, 3, 4, 5];
-      testElements.forEach(el =>
-        SubmitTaskManager.createTask(el.toString(), el)
-      );
-      const tasks = SubmitTaskManager.tasks;
+      testElements.forEach(el => {
+        SubmitTaskManager.getInstance().createTask(el.toString(), el);
+      });
+      const tasks = SubmitTaskManager.getInstance().tasks;
       assert.equal(tasks.length, 3);
-      testElements
-        .slice(-3)
-        .reverse()
-        .forEach((el, index) => assert.equal(tasks[index][0], el));
+      testElements.slice(-3).forEach((el, index) => {
+        assert.equal(tasks[index][0], el);
+      });
     });
   });
 });
