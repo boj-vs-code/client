a7dd25034db2586630a60f7b225fdd43fc485822
moreal <dev.moreal@gmail.com>
2019-03-14 12:24:00 +0900

    Add action to register views

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,15 @@ import { submitSubscription } from "./subscriptions/submit";
 import { showProblemInformationSubscription } from "./subscriptions/problem";
 import { showSubmitTasksSubscription } from "./subscriptions/tasks";
 import { ViewManager } from "./views";
+import { SubmitTasksView } from "./views/tasks";
+import { WelcomeView } from "./views/welcome";
+import { ProblemView } from "./views/problem";
 
 export function activate(context: vscode.ExtensionContext) {
+  ViewManager.registerView(new WelcomeView());
+  ViewManager.registerView(new ProblemView());
+  ViewManager.registerView(new SubmitTasksView());
+
   ViewManager.show("WELCOME_VIEW");
 
   context.subscriptions.push(submitSubscription);
