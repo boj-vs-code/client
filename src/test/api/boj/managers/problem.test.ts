5f061cfd1870ec8b07eb14440ea1fdd9b7da8dab
moreal <dev.moreal@gmail.com>
2019-03-18 23:34:36 +0900

    Add ProblemManager testcode

---
+++ src/test/api/boj/managers/problem.test.ts
@@ -0,0 +1,35 @@
+import * as assert from "assert";
+import { ProblemManager } from "../../../../api/boj/managers/problem";
+import { Problem } from "../../../../api/boj/problem";
+
+suite("BOJ API TEST at api/boj/managers/problem", () => {
+  suite("ProblemManager#recent", () => {
+    test("when first, will return undefined", () => {
+      assert.equal(undefined, ProblemManager.getInstance().recent);
+    });
+
+    test("after getting problem, recent will return the problem instance", () => {
+      ProblemManager.getInstance().getProblem(1000);
+      assert.equal((<Problem>ProblemManager.getInstance().recent).title, "A+B");
+    });
+  });
+
+  suite("ProblemManager#getProblem", () => {
+    test("(1000)", () => {
+      const expected = {
+        title: "A+B",
+        description:
+          "두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.",
+        inputDescription: "첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)",
+        outputDescription: "첫째 줄에 A+B를 출력한다."
+      } as Problem;
+
+      const actual = <Problem>ProblemManager.getInstance().getProblem(1000);
+
+      assert.equal(actual.title, expected.title);
+      assert.equal(actual.description, expected.description);
+      assert.equal(actual.inputDescription, expected.inputDescription);
+      assert.equal(actual.outputDescription, expected.outputDescription);
+    });
+  });
+});
