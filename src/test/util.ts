1cad748d2a8c82b286a7a321c66edf9e8e702f74
moreal <dev.moreal@gmail.com>
2019-03-18 16:04:27 +0900

    mAdd function 'makeRandomString'

--- src/test/util.ts
+++ src/test/util.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import { randomBytes } from "crypto";
 
 export function createMockAdapter() {
   return new MockAdapter(axios);
+}
+
+export function makeRandomString(length: number): string {
+  return randomBytes(length / 2).toString("hex");
 }
