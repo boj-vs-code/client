a03815593baa0b2eb29c5eccdf0731e3ad37129d
moreal <dev.moreal@gmail.com>
2019-03-25 08:54:07 +0900

    Add gulp task to clean problems

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 "use strict";
 
-var gulp = require("gulp");
+const gulp = require("gulp");
+const del = require("del");
 
-gulp.task("move-resources", function() {
+gulp.task("move:resources", function() {
   return gulp
     .src(["./src/test/fixtures/**/*", "./src/test/fixtures/**/.*"])
     .pipe(gulp.dest("./out/test/fixtures/"));
 });
+
+gulp.task("clean:problems-cache", function() {
+  return del(["./resources/problems/*"]);
+});
+
+gulp.task("default", gulp.series("move:resources", "clean:problems-cache"));
