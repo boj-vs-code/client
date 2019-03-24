adfe8f51df9d7a0ad8a2c70522b20f6599b7a92e
moreal <dev.moreal@gmail.com>
2019-03-24 19:46:10 +0900

    Update testcode api/boj/managers/problem

--- src/test/api/boj/managers/problem.test.ts
+++ src/test/api/boj/managers/problem.test.ts
@@ -8,14 +8,14 @@ suite("BOJ API TEST at api/boj/managers/problem", () => {
       assert.equal(undefined, ProblemManager.getInstance().recent);
     });
 
-    test("after getting problem, recent will return the problem instance", () => {
-      ProblemManager.getInstance().getProblem(1000);
+    test("after getting problem, recent will return the problem instance", async () => {
+      await ProblemManager.getInstance().getProblem(1000);
       assert.equal((<Problem>ProblemManager.getInstance().recent).title, "A+B");
     });
   });
 
   suite("ProblemManager#getProblem", () => {
-    test("(1000)", () => {
+    test("(1000)", async () => {
       const expected = {
         title: "A+B",
         description:
@@ -24,7 +24,28 @@ suite("BOJ API TEST at api/boj/managers/problem", () => {
         outputDescription: "첫째 줄에 A+B를 출력한다."
       } as Problem;
 
-      const actual = <Problem>ProblemManager.getInstance().getProblem(1000);
+      const actual = <Problem>(
+        await ProblemManager.getInstance().getProblem(1000)
+      );
+
+      assert.equal(actual.title, expected.title);
+      assert.equal(actual.description, expected.description);
+      assert.equal(actual.inputDescription, expected.inputDescription);
+      assert.equal(actual.outputDescription, expected.outputDescription);
+    });
+
+    test("(1001)", async () => {
+      const expected = {
+        title: "A-B",
+        description:
+          "두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.",
+        inputDescription: "첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)",
+        outputDescription: "첫째 줄에 A-B를 출력한다."
+      } as Problem;
+
+      const actual = <Problem>(
+        await ProblemManager.getInstance().getProblem(1000)
+      );
 
       assert.equal(actual.title, expected.title);
       assert.equal(actual.description, expected.description);
