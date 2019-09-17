8eaae67ac40ee69ab120e746697c5d713e6bccfb
Moreal <dev.moreal@gmail.com>
2019-09-18 03:15:55 +0900

    add: Executor testcode

---
+++ src/test/executor.test.ts
@@ -0,0 +1,18 @@
+import * as assert from "assert";
+import { Executor } from "../executors";
+
+suite("Executor test code with input, output", () => {
+  suite("Executor#python", () => {
+    test("should be failed", () => {
+      const executor = new Executor('', 'python $BOJ_SOURCE_FILE');
+      const result = executor.test(__dirname + '/fixtures/solutions/solve.py', ['2\n', '1\n']);
+      assert.deepStrictEqual(result, [false, '2\n', '1\n', '2\n']);
+    });
+
+    test("should be success", () => {
+      const executor = new Executor('', 'python $BOJ_SOURCE_FILE');
+      const result = executor.test(__dirname + '/fixtures/solutions/solve.py', ['1\n', '1\n']);
+      assert.deepStrictEqual(result, [true, '', '', '']);
+    });
+  });
+});
