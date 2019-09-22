7d59078483c2e1e0410e8f8ed4903d2e0bc40dd9
Moreal <dev.moreal@gmail.com>
2019-09-23 01:51:09 +0900

    update: refactor and add more cases

--- src/test/executors/manager.test.ts
+++ src/test/executors/manager.test.ts
@@ -1,24 +1,27 @@
 import * as assert from "assert";
 import { ExecutorManager } from "../../executors/manager";
+import { Executor } from "../../executors";
 
 suite("ExecutorManager", () => {
-  suite("Python 2", () => {
-    test("should be failed", () => {
-      const executor = ExecutorManager.get('Python 2');
-      if (executor === undefined) {
-          return;
-      }
-      const result = executor.test(__dirname + '/../fixtures/solutions/solve.py', ['2\n', '1\n']);
-      assert.deepStrictEqual(result, [false, '2\n', '1\n', '2\n']);
-    });
+  const testcases = [
+    ["Python 2", "solve.py"],
+    ["Python 3", "solve.py"],
+    ["C", "solve.c"]
+  ];
 
-    test("should be success", () => {
-      const executor = ExecutorManager.get('Python 2');
-      if (executor === undefined) {
-          return;
-      }
-      const result = executor.test(__dirname + '/../fixtures/solutions/solve.py', ['1\n', '1\n']);
-      assert.deepStrictEqual(result, [true, '', '', '']);
+  testcases.forEach(([languageName, sourceFile]) => {
+    suite(languageName, () => {
+      test("should be failed", () => {
+        const executor = <Executor>ExecutorManager.get(languageName);
+        const result = executor.test(__dirname + `/../fixtures/solutions/${sourceFile}`, ['2\n', '1\n']);
+        assert.deepStrictEqual(result, [false, '2\n', '1\n', '2\n']);
+      });
+  
+      test("should be success", () => {
+        const executor = <Executor>ExecutorManager.get(languageName);
+        const result = executor.test(__dirname + `/../fixtures/solutions/${sourceFile}`, ['1\n', '1\n']);
+        assert.deepStrictEqual(result, [true, '', '', '']);
+      });
     });
   });
 });
